0c8dcf695033ed7fc4b8b623b2940470b2bb6fa4
edc1568 <1983634526@qq.com>
2024-05-09 20:29:29 +0800

    1

---
+++ 11.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	long f1;
+	long f2;
+	f1 = f2 = 1;
+	for (int i = 1; i <= 20; i++)
+	{
+		cout << "\t"<<f1 <<"\t"<< f2;
+		if (i % 2 == 0)
+		{
+			cout << "\n" ;
+		}
+		f1 = f2 + f1;
+		f2 = f1 + f2;
+	}
+	getchar();//第一次修改。
+
+	//////////
+111111111
+	return 0;
+}
